b640ce974041b30ddaa7456fc00c84a6d7f58dd6
Sushank Saini <sushank.saini@dal.ca>
2025-04-04 15:45:38 -0600

    Python Problem Solving 04.04.2025

--- gemstones/bettersolution.py
+++ gemstones/bettersolution.py
@@ -1,10 +1,15 @@
 # refer: https://www.youtube.com/watch?v=vcwFPaZuAHI
 def gemstones(arr):
     result=set.intersection(*map(set,arr)) 
-    # map(set,arr) will convert each string into a set
-    # *map(set,arr) will convert map() object in to separate sets
+    # map(set,arr) applies the set() constructor to each element in arr 
+    # which will convert each string into a map object that contains
+    # sets like <set('abc'),set('bbc')>
+    # *map(set,arr), * operator unpacks the map() object into individual arguments.
+    # So,set.intersection(*map(set, arr)) becomes set.intersection(set('abc'), set('bbc'))
     # set.intersection(*map(set,arr)) will return a set that have common values (minerals) in all the sets (rocks)
     # these common minerals are our gemstones
+    #print(*map(set,arr))
+    #print(result)
     return len(result)
 if __name__ == '__main__':
     n = int(input().strip())
