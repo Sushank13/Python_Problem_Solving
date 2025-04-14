a175ea2cc8f13a1d9b36161bfc2c647f96b51674
Sushank Saini <sushank.saini@dal.ca>
2025-04-13 18:38:26 -0600

    Python Problem Solving3 13.04.2025

---
+++ remove_duplicates_in_list/code.py
@@ -0,0 +1,12 @@
+# WAP to remove duplicates from a list preserving the order.
+#[b,c,c,a,c,b,a]->[b,c,a]
+# iterate the list-> add elements in a list after checking if element does not exist in set
+def remove_duplicates(input_list):
+    tracker=set() # using set() as time complexity of lookups is O(1)
+    output=[] # need list as order is to be preserved
+    for item in input_list:
+        if item not in tracker:
+            tracker.add(item)
+            output.append(item)
+    return output 
+print(remove_duplicates(["b","c","c","a","c","b","a"]))
