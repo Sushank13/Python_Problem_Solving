45e008549510ad6d9a1da4df2ffa9f178e3f5ea0
Sushank Saini <sushank.saini@dal.ca>
2025-04-17 14:20:08 -0600

    Python Problem Solving 17.04.2025

---
+++ leetcode-14/Longest-Common-Prefix.py
@@ -0,0 +1,8 @@
+# leetcode-14 WAP for Longest Common Prefix
+# Given an array of strings, 
+# find the longest common prefix string amongst them.
+#If there is no common prefix, return an empty string ""
+# Input1: ["flower", "flow", "flight"]
+#Output1: "fl"
+#Input2: ["dog", "racecar", "car"]
+#Output2: ""
