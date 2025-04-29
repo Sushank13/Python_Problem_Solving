f5142d7e4989bf61d187879f3ee2bb688415e05e
Sushank Saini <sushank.saini@dal.ca>
2025-04-29 02:01:02 -0600

    Python Problem Solving 29.04.2025

--- binary_trees/binary_tree_traversals.py
+++ binary_trees/binary_tree_traversals.py
@@ -20,30 +20,65 @@ root.right=node3
 node2.left=node4
 node2.right=node5
 
-#pre-order traversal using recursion
+#DFS:pre-order traversal using recursion
 def pre_order_traversal(node):
     if node: # if node is not None
         print(node.data,end=" ")
         pre_order_traversal(node.left)
         pre_order_traversal(node.right)
         
-#in-order using recursion
+#DFS:in-order using recursion
 def in_order_traversal(node):
     if node:
         in_order_traversal(node.left)
         print(node.data,end=" ")
         in_order_traversal(node.right)
 
-#post-order using recursion
+#DFS:post-order using recursion
 def post_order_traversal(node):
     if node:
         post_order_traversal(node.left)
         post_order_traversal(node.right)
         print(node.data,end=" ")
-
+        
+# breadth first search traversal
+from collections import deque
+def bfs(root):
+    if not root: # tree is empty
+        return None
+    q=deque()
+    q.append(root)
+    while q:
+        current=q.popleft()
+        print(current.data,end=" ")
+        if current.left is not None:
+            q.append(current.left)
+        if current.right is not None:
+            q.append(current.right)
+            
+def bfs_levels_newline(root):
+    if not root:
+        return None
+    q=deque()
+    q.append(root)
+    while q:
+        level_size=len(q)
+        for _ in range(level_size):
+            current=q.popleft()
+            print(current.data,end=" ")
+            if current.left is not None:
+                q.append(current.left)
+            if current.right is not None:
+                q.append(current.right)
+        print()
+                   
 #function calls
 pre_order_traversal(root) # output: 10 5 2 7 15 (tree copying)
 print()
 in_order_traversal(root) # output: 2 5 7 10 15 (sorted data)
 print()
 post_order_traversal(root) # output: 2 7 5 15 10
+print()
+bfs(root)
+print()
+bfs_levels_newline(root)
