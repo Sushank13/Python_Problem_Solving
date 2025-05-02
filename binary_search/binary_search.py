8bdb582d99c5956ecdf1103adcf7d4651359cd38
Sushank Saini <sushank.saini@dal.ca>
2025-05-01 19:28:50 -0600

    Python Problem Solving2 01.05.2025

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,19 +1,24 @@
-# input: [3,10,2,5,39,1], return index of 39 
+# input: [3,10,2,5,39,1] unsorted array, return index of 39 
 # output: index of element 
 # first sort the list [1,2,3,5,10,39]
+# input validation->empty list is provided
 # edge case-> element does not exist in the list
 def binary_search(input_list,target):
-    input_list.sort() # sort list in ascending order
-    start,end=0,len(input_list)-1
+    if not input_list:
+        return "Empty Input List"
+    input_list.sort() # sort the array
+    start=0
+    end=len(input_list)-1
     while(start<=end):
-        middle=(start+end)//2
-        if input_list[middle]==target:
-            return middle
-        elif target>input_list[middle]: # target is on the right side
-            start=middle+1
-        elif target<input_list[middle]:#target is on the left side
-            end=middle-1 
-    return -1 # element does not exist
+        mid =(start+end)//2
+        if target<input_list[mid]: # value is in left side
+            end=mid-1
+        elif target>input_list[mid]: # value is in right side
+            start=mid+1
+        else: # match found
+            return mid
+    return -1 # value does not exist 
+print(binary_search([],2))
 print(binary_search([3,10,2,5,39,1],39)) # output: 5
 print(binary_search([3,10,2,5,39,1],10)) # output: 4
 print(binary_search([3,10,2,5,39,1],3)) # output: 2
